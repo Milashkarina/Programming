fe1c0de6e67f14dcaa8e8fac2d8bf4e1dfc5b064
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-09-27 19:48:12 +0300

    Загрузка первого задания

---
+++ 1.1/1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace OuputUser
+{
+    class Program
+    {
+        //Проверка является ли число простым
+        
+        static bool isPrime(int j)
+        {
+           for(int i=2;i*i<=j;i++)
+                {
+                if (j % i == 0)
+                    return false;
+                }
+           return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n;
+            n = Int32.Parse(Console.ReadLine());
+            Console.Write("Простые числа меньшие {0}:", n);
+            for (int i = 2; i < n; i++)
+            {
+                if (isPrime(i))
+                    Console.Write(" {0}", i);
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
