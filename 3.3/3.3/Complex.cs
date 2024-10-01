29e5360d5956615e88aa29d6c81e3d03f18ec480
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-01 23:19:30 +0300

    Добавила задание 3.3

---
+++ 3.3/3.3/Complex.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    class Complex
+
+        // Вводим вещественные переменные real и image
+    {
+        double _real;
+        double _image;
+
+        public Complex(double r, double i)
+        {
+            _real = r;
+            _image = i;
+        }
+
+        // Создаем модификатор,который возвращает сумму комплексных чисел
+        public static Complex Add(Complex z1, Complex z2)
+        {
+            return new Complex(z1._real + z2._real, z1._image + z2._image);
+        }
+
+        public static Complex operator +(Complex z1, Complex z2)
+        {
+            return Add(z1, z2);
+        }
+
+        // Создаем модификатор,который возвращает разницу комплексных чисел
+        public static Complex Substract(Complex z1, Complex z2)
+        {
+            return new Complex(z1._real - z2._real, z1._image - z2._image);
+        }
+
+        public static Complex operator -(Complex z1, Complex z2)
+        {
+            return Substract(z1, z2);
+        }
+
+        // Создаем модификатор,который возвращает произведение комплексных чисел
+        public static Complex Multiply(Complex z1, Complex z2)
+        {
+            return new Complex(
+                z1._real * z2._real - z1._image + z2._image,
+                z1._real * z2._image + z2._real * z1._image);
+        }
+
+        public static Complex operator *(Complex z1, Complex z2)
+        {
+            return Multiply(z1, z2);
+        }
+
+        public double Abs()
+        {
+            return Math.Sqrt(_real * _real + _image * _image);
+        }
+
+        public override string ToString()
+        {
+            return $"{_real}+{_image}i";
+        }
+    }
+}
---
+++ 3.3/3.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using ComplexNumbers;
+
+var z1 = new Complex(2, 3);
+var z2 = new Complex(9, 7);
+
+
+
+Console.WriteLine(z1);
+Console.WriteLine(z2);
+Console.WriteLine(Complex.Add(z1, z2));
+Console.WriteLine(z1 + z2);
+Console.WriteLine(z1.Abs());
+
+
+Console.ReadKey();
