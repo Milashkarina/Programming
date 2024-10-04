e9cb2da023445fc6c1810762fd7b99ee8856ddb2
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-04 11:53:06 +0300

    Первое_задание_кр_1

---
+++ Контрольная_работа_1_1/Контрольная_работа_1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OutPut
+{
+    public class Program
+    {
+        static double Factorial(int n)
+        {
+
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return Factorial(n - 1) * n;
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            double x = Convert.ToUInt32(Console.ReadLine());
+            double rez = x - ((Math.Pow(x, 3)) / (Factorial(3))) + ((Math.Pow(x, 5)) / (Factorial(5))) - ((Math.Pow(x, 7)) / (Factorial(7))) + ((Math.Pow(x, 9)) / (Factorial(9))) - ((Math.Pow(x, 11)) / (Factorial(11))) + (((Math.Pow(x, 13)) / (Factorial(13))));
+            Console.WriteLine($"Результат выражения  равен {rez}");
+
+        }
+    }
+}
