d2e76bf29f7ac8b5ff4ced4796d769fb8b23055f
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-04 12:09:53 +0300

    Факториал_рекурсия

---
+++ Факториал_рекурсия/Факториал_рекурсия/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static long Factorial(long n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return Factorial(n - 1) * n;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("n = ");
+        long n = (long)Convert.ToUInt32(Console.ReadLine());
+        Console.WriteLine($"Факториал числа {n} равен {Factorial(n)}");
+
+    }
+}
