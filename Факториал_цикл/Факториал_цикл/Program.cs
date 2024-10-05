7920f3178ca36ef5203f09b45aae8e25cad53063
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-05 11:54:44 +0300

    Добавила задание факториал цикл

---
+++ Факториал_цикл/Факториал_цикл/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Program
+{
+    class FactorialTer
+    {
+        private static long F(long n)
+        {
+            long rez = 1;
+                for (long i = 1; i <= n; i++)
+                {
+                     rez *=i;
+                }
+                return rez;
+
+        }
+            static void Main(string[] args)
+        {
+                Console.Write("n = ");
+                long n = Convert.ToUInt32(Console.ReadLine());
+                Console.WriteLine($"Факториал числа {n} равен {F(n)}");
+               
+        }
+    }
+}
+
+
