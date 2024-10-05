954868b5e98d97cdc56c7ed39aa197951e200c8f
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-05 18:33:04 +0300

    Задание_Фибоначчи_рекурсия

---
+++ Фибоначчи_рекурсия/Фибоначчи_рекурсия/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    private static long Fib(long n)
+    {
+        if (n == 0|| n == 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fib(n - 1)+Fib(n-2);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("n = ");
+        long n = (long)Convert.ToUInt32(Console.ReadLine());
+        Console.WriteLine($"Число Фибоначчи {n} : {Fib(n)}");
+
+    }
+}
