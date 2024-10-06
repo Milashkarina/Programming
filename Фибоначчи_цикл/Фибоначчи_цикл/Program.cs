b1114a2d75609fdc40c388efc65ea3afbda4f6df
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-06 20:38:43 +0300

    Добавила задание Фибоначчи_цикл

---
+++ Фибоначчи_цикл/Фибоначчи_цикл/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Program
+{
+    class FactorialTer
+    {
+        private static long Fub(long n)
+        {
+            long num1 = 0;
+            long num2= 1;
+            long rez = 0;
+            for (long i = 2; i <= n; i++)
+            { // находим сумму двух последних чисел
+                rez = num1 + num2;
+              //переходим к следующим числам
+                num1 = num2;
+                num2 = rez;
+
+
+            }
+            return rez;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            long n = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine($"Число Фибоначчи под номером  {n} равен {Fub(n)}");
+
+        }
+    }
+}
+
+
+
