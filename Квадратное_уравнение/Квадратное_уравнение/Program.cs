a1a27145693a06671469d04162ee6de29389a519
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-08 01:58:18 +0300

    Добавила задние Квадратное_уравнение

---
+++ Квадратное_уравнение/Квадратное_уравнение/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class FactorialTer
+{
+   static void Main(string[] args)
+   {
+     double x1;
+     double x2;
+     double D;
+     double E=0.0001;
+
+    Console.Write("Введите коэффициент a: ");
+    double a = Convert.ToSingle(Console.ReadLine());
+    Console.Write("Введите коэффициент b: ");
+    double b = Convert.ToSingle(Console.ReadLine());
+    Console.Write("Введите коэффициент c: ");
+    double c = Convert.ToSingle(Console.ReadLine());
+
+    D = Math.Pow(b, 2) - 4 * a * c;
+
+     if (D > 0 & D > E)
+      {
+       x1 = (-b + Math.Sqrt(D)) / (2 * a);
+       x2 = (-b - Math.Sqrt(D)) / (2 * a);
+       Console.WriteLine($"Уравнение имеет два решения: {x1}, {x2}");
+                
+       }
+       else if ( D < 0 & D < -E)
+       {
+            Console.WriteLine($"Уравнение не имеет решение");
+        }
+       else
+       {
+        x1 = -b / (2 * a);
+        Console.WriteLine($"Уравнение имеет один корень:{x1}");
+            
+       }
+   }
+}
+
