1bf1794b66cdab781b46d24f4959fceaa72c8e82
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-11 12:26:06 +0300

    Добавила

--- 3.3/3.3/Complex.cs
+++ 3.3/3.3/Complex.cs
@@ -50,11 +50,39 @@ namespace ComplexNumbers
             return Multiply(z1, z2);
         }
 
+        // Создаем модификатор,который возвращает деление комплексных чисел
+        public static Complex Division(Complex z1, Complex z2)
+        {
+            return new Complex((z1._real * z2._real + z1._image * z2._image) / (z2._real * z2._real + z2._image * z2._image), (z2._real * z1._image - z1._real * z2._image) / (z2._real * z2._real + z2._image * z2._image));
+        }
+
+        public static Complex operator /(Complex z1, Complex z2)
+        {
+            return Division(z1, z2);
+        }
+
+        // Создаем модификатор,который возвращает  возведение в степень комплексных чисел
+        public Complex Power(int n)
+        {
+            Complex result = new Complex(1, 0);
+            for (int i = 0; i < n; i++)
+            {
+                result = result * new Complex(_real, _image);
+            }
+            return result;
+        }
+
+        // Создаем модификатор,который возвращает извлечение корня из числа комплексных чисел
         public double Abs()
         {
             return Math.Sqrt(_real * _real + _image * _image);
         }
 
+        // Создаем модификатор,который возвращает значение угла комплексных чисел
+        public double Angle()
+        {
+            return Math.Atan(_image / _real);
+        }
         public override string ToString()
         {
             return $"{_real}+{_image}i";
--- 3.3/3.3/Program.cs
+++ 3.3/3.3/Program.cs
@@ -5,11 +5,22 @@ var z2 = new Complex(9, 7);
 
 
 
-Console.WriteLine(z1);
-Console.WriteLine(z2);
+Console.WriteLine($"Первое  число:{z1}");
+Console.WriteLine($"Второе комплексное число:{z2}");
 Console.WriteLine(Complex.Add(z1, z2));
-Console.WriteLine(z1 + z2);
-Console.WriteLine(z1.Abs());
+
+Console.WriteLine($"Сумма комплексных чисел:{z1 + z2}");
+Console.WriteLine($"Разность комплексных чисел: {z1 - z2}");
+Console.WriteLine($"Произведение комплексных чисел:{z1 * z2}");
+Console.WriteLine($"Деление комплексных чисел:{z1 / z2}");
+Console.WriteLine($"Возведение комплексного числа в степень:{z1.Power(4)}");
+Console.WriteLine($"Нахождение угла:{z1.Angle()}");
+Console.WriteLine($"Модуль комплексного числа:{z1.Abs()}");
 
 
 Console.ReadKey();
+
+
+    
+
+    
