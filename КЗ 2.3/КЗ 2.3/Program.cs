ee9d938ad297724218089292364350a9cb173649
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-12 19:58:14 +0300

    Добавила КЗ 2.3

---
+++ КЗ 2.3/КЗ 2.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Program
+{
+    class Hypothesis
+    {
+
+        static void Main(string[] args)
+        {
+           
+            Console.Write($"Введите n:");
+            int n = Convert.ToInt32(Console.Read());
+            int count = 0;
+
+            if (n > 0)
+            {
+                do
+                {
+                    if (n % 2 == 0)
+                    {
+                        n = n / 2;
+                    }
+                    else
+                    {
+                        n = 3 * n + 1;
+                    }
+                    count++;
+                }
+                while (n != 1);
+                Console.Write($"Количество замен, приводящих к 1:{count}");
+            }
+            else
+            {
+                Console.Write("Либо Вы ввели 0,либо отрицательной число");
+            }
+           
+        }
+    }
+}
