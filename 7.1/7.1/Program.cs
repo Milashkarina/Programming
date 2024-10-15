23fa264ec22d66180b35664bff9fd7afc3f9c48b
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-15 23:02:22 +0300

    КЗ 7.1

---
+++ 7.1/7.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Linq;
+namespace Program
+{
+    class MyProgram
+    {
+        public static void Main(string[] args)
+        {
+
+
+            try
+            {
+                Console.Write("Введите значение n: ");
+                
+                int n = (int)Convert.ToUInt32(Console.ReadLine());
+                Console.Write($"Результат: {n}");
+
+                
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено не число");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Ошибка:Число либо слишком большое,либо маленькое.Будьте внимательны!");
+            }
+
+        }
+    }
+}
+
+
+
