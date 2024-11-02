70fa1d87aac4622448099db232c950fa512d9454
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-11-02 23:06:03 +0300

    11.1

---
+++ 11.1/11.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+
+class Test
+{
+     static void Main(string[] args)
+    {   //Указываем путь файла 
+        string path = @"..\..\f.txt";
+        {
+            // Создаем файл
+            using (StreamWriter s = File.CreateText(path))
+            {
+                // Записываем в него  строку
+                s.WriteLine("x sin(x)");
+                //Вычисляем значение sin с заданным шагом и записываем вычисления  в файл
+                for (double i = 0; i <= 1.01f; i += 0.1f)
+                {
+                    double sin = Math.Sin(i);
+
+                    s.WriteLine($"{i},{sin}");
+                }
+
+            }
+        }
+
+        Console.WriteLine("Данные успешно записаны в файл");
+    }
+}
